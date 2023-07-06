30df1fe6d091e4402640824b3a3b647525d688a2
shubham mandrawal <shubhammandrawal2826@gmail.com>
2023-07-06 11:38:32 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect , useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [time, setTime] = useState(0);
+
+  const handleClick = (e) =>{
+    if(e.key === "Enter"){
+      const inputTime = Math.floor(e.target.value);
+      if(isNaN(inputTime)){
+        setTime(0)
+      } else{
+        setTime(inputTime)
+      }
+    }
+  }
+
+  useEffect(()=>{
+    let intervalId;
+    if(time > 0){
+      intervalId = setInterval(()=>{
+        setTime(prevTime => prevTime - 1);
+      }, 1000)
+    }
+
+    return () => clearInterval(intervalId);
+  }, [time])
+
+  return <div className="App">
+    <input type="text" onKeyDown={handleClick} />
+    <div>{time}</div>
+  </div>
 }
 
 export default App;
